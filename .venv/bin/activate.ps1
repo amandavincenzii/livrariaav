c9bdfe09a5ba4211ac9e855428dee73c5079ac92
Amanda Vincenzi <amandavincenzi.ifc@gmail.com>
2022-10-20 12:57:21 -0300

    commit14

--- .venv/bin/activate.ps1
+++ .venv/bin/activate.ps1
@@ -42,11 +42,11 @@ if (!$env:VIRTUAL_ENV_DISABLE_PROMPT) {
     }
     $function:_old_virtual_prompt = $function:prompt
 
-    if ("" -ne "") {
+    if ("livrariaav-py3.8" -ne "") {
         function global:prompt {
             # Add the custom prefix to the existing prompt
             $previous_prompt_value = & $function:_old_virtual_prompt
-            ("" + $previous_prompt_value)
+            ("(livrariaav-py3.8) " + $previous_prompt_value)
         }
     }
     else {
